54c7e0a8747fc10c93c79726879346a2150c7213
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2026-08-21 22:01:49 +0700

    chore: scaffold job repository and service layers

---
+++ app/repositories/jobs.py
@@ -0,0 +1,12 @@
+class JobRepository:
+    def select(self) -> list:
+        return [];
+
+    def create(self, data: dict):
+        pass
+
+    def update(self, id: str, data: dict):
+        pass
+
+    def delete(self, id: str):
+        pass
